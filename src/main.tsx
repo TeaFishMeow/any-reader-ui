4d3068af5efc0f6126594ee933ec81d9fd359a09
sprite <ximingzheng@smail.nju.edu.cn>
2026-05-15 19:24:18 +0800

    refactor: initialize theme application in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
+import { applyTheme, themeMode, themeStyle } from './lib/theme'
 import 'katex/dist/katex.min.css'
 import './index.css'
+
+applyTheme(themeMode(), themeStyle())
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<App />)
